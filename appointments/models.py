3ecda88a65501aeafbb2fa0e4f161a11a3e590bc
jaiber430 <jaiber4302@gmail.com>
2026-06-11 23:48:32 -0500

    New app and appointment model

---
+++ appointments/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from users.models import User, Post
+
+# Create your models here.
+class Quote(models.Model):
+
+    # Values for the states
+    STATE_CHOICES = [
+        ('pending', 'Pendiente'),
+        ('confirmed', 'Confirmada'),
+        ('cancelled', 'Cancelada'),
+        ('completed', 'Completada')
+    ]
+
+    patient = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='quote_patient'
+    )
+
+    doctor = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='quote_doctor'
+    )
+
+    # By default, any appointment starts in pending status
+    state = models.CharField(max_length=20, choices=STATE_CHOICES, default='pending')
+
+    # Specialty of the appointment
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='quote_post'
+    )
+
+    description = models.TextField(blank=True, null=True)
+
+    appointment_date = models.DateTimeField()
+
+    class Meta:
+        db_table = 'quote'
+
+    def __str__(self):
+        return f'{self.patient} - {self.doctor} - {self.appointment_date}'
