eda3938a61440ba925dfa7025f4ae82ca931ca41
jaiber430 <jaiber4302@gmail.com>
2026-06-30 22:35:51 -0500

    ViewSet

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from appointments.models import Diagnosis, Quote
+from appointments.serializers import DiagnosisSerializer, QuoteSerializer
 
 # Create your views here.
+class DiagnosisViewSet(viewsets.ModelViewSet):
+    queryset = Diagnosis.objects.all()
+    serializer_class = DiagnosisSerializer
+
+class QuoteViewSet(viewsets.ModelViewSet):
+    queryset = Quote.objects.all()
+    serializer_class = QuoteSerializer
--- users/views.py
+++ users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from users.models import User, Role, Post, UserRole, Schedule, DoctorSpecialty
+from users.serializers import UserSerializer, RoleSerializer, PostSerializer, UserRoleSerializer, ScheduleSerializer, DoctorSpecialtySerializer
 
 # Create your views here.
+
+# ModelViewSet => All http methods
+class UserViewSet(viewsets.ModelViewSet):
+    # Data handled by the view
+    queryset = User.objects.all()
+    # Convert data to Json
+    serializer_class = UserSerializer
+
+class RoleViewSet(viewsets.ModelViewSet):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserRoleViewSet(viewsets.ModelViewSet):
+    queryset = UserRole.objects.all()
+    serializer_class = UserRoleSerializer
+
+class ScheduleViewSet(viewsets.ModelViewSet):
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+
+class DoctorSpecialtyViewSet(viewsets.ModelViewSet):
+    queryset = DoctorSpecialty.objects.all()
+    serializer_class = DoctorSpecialtySerializer
